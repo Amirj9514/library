e54ee78a02bfc8a039f0d36a0c33e25b7dd4c58a
Amir <amirj9514@gmail.com>
2023-01-03 15:47:57 +0500

    new change

--- lib_project/asgi.py
+++ lib_project/asgi.py
@@ -5,6 +5,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+sksfkbsknksdklsf
 """
 
 import os
@@ -14,3 +15,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lib_project.settings")
 
 application = get_asgi_application()
+ 
